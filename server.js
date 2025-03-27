680ca3870349b95f4aa66ef7d2d46fcedfc1db09
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-27 18:42:00 +1100

    Add cors

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from "express";
+import cors from "cors"
 
 const app = express();
-const PORT = 8000
+const PORT = 8000;
 
 // middleware
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, (error) => {
     error   
         ? console.log("Error:", error.message)
         : console.log(`Server started successfully at PORT:${PORT}`);
-        
-        
 })
