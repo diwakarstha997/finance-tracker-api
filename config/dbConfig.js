92b7c88bf844472495a8ee440444d4dcd78856ab
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-29 22:09:54 +1100

    Add db name on dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDb = () => {
     try {
-        const connect = mongoose.connect(process.env.DB_CONNECT_URL);
+        const connect = mongoose.connect(process.env.DB_CONNECT_URL + process.env.DB_NAME);
         
         if(connect) {
             console.log("Database Connected:", process.env.DB_CONNECT_URL);
