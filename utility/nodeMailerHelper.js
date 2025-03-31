04579188ae7ed6b523a6e86adda615c455b5e45c
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-31 20:15:15 +1100

    Configure mail service using nodemailer

---
+++ utility/nodeMailerHelper.js
@@ -0,0 +1,11 @@
+import nodemailer from "nodemailer";
+
+// configure transporter
+const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+})
