fe2dcfb2e48dcaba0e76d3c87a5ed1539c0ff8b5
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-04-02 19:33:10 +1100

    Create verify user email endpoint

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -4,7 +4,6 @@ import nodemailer from "nodemailer";
 const transport = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
