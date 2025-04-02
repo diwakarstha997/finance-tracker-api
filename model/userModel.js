6a45407ac039989266fb0fece4880af304b09591
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-04-02 18:35:28 +1100

    Add updateUser operation to user model

--- model/userModel.js
+++ model/userModel.js
@@ -7,3 +7,16 @@ const userModel = mongoose.model("user", userSchema);
 export const createUser = (userObj) => {
     return userModel(userObj).save();
 }
+
+// verify user: filter - used to find user, updatedUser - user updated data
+export const updateUser = (filter, updatedUser) => {
+    return userModel
+                .findOneAndUpdate(
+                    filter, 
+                    updatedUser, 
+                    {   
+                        // returns document update is applied
+                        new: true
+                    }
+                );
+}
