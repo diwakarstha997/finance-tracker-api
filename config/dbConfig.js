e4f1fa6f95764a11fcef389fa5a2c4273346373d
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-04-03 18:43:06 +1100

    Update dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectToMongoDb = () => {
     try {
-        const connect = mongoose.connect(process.env.DB_CONNECT_URL + process.env.DB_NAME);
+        const connect = mongoose.connect(process.env.DB_CONNECT_URL);
         
         if(connect) {
-            console.log("Database Connected:", process.env.DB_CONNECT_URL +  process.env.DB_NAME);
+            console.log("Database Connected:", process.env.DB_CONNECT_URL);
         } 
     } catch (error) {
         console.log("Error Connecting to Database:", error);
