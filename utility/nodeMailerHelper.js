7f67772a67848e9f3fd63244cc8bcc41ce81a5db
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-04-11 19:18:30 +1000

    Hot Fix: Email Verification click issue

--- utility/nodeMailerHelper.js
+++ utility/nodeMailerHelper.js
@@ -12,9 +12,9 @@ export const sendEmailVerification = (email, verificationUrl) => {
          <div style="max-width: 600px; margin: 20px auto; background: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
             <h2 style="color: #333;">Email Verification</h2>
             <p style="color: #555; font-size: 16px;">Thank you for signing up with <strong>FinanceTracker</strong>! To complete your registration, please verify your email address by clicking the button below:</p>
-            <a href="[Verification Link]" style="display: inline-block; padding: 12px 24px; margin: 20px 0; text-decoration: none; color: #ffffff; background-color: #007bff; border-radius: 5px; font-size: 16px;">Verify My Email</a>
+            <a href="${verificationUrl}" style="display: inline-block; padding: 12px 24px; margin: 20px 0; text-decoration: none; color: #ffffff; background-color: #007bff; border-radius: 5px; font-size: 16px;">Verify My Email</a>
             <p style="color: #555; font-size: 16px;">If the button doesn’t work, copy and paste the following link into your browser:</p>
-            <p style="word-wrap: break-word;"><a href="[Verification Link]" style="color: #007bff; text-decoration: none;">${verificationUrl}</a></p>
+            <p style="word-wrap: break-word;"><a href="${verificationUrl}" target="_blank" style="color: #007bff; text-decoration: none;">${verificationUrl}</a></p>
             <p style="color: #555; font-size: 16px;">If you didn’t sign up for an account, you can safely ignore this email.</p>
             <div style="margin-top: 20px; font-size: 14px; color: #777;">
                 <p>Best regards,</p>
