c1781803a2fda36d1f52560704c3ac29d58ca310
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-05-07 15:32:10 +1000

    Update transaction schema

--- routers/tranasactionRouter.js
+++ routers/tranasactionRouter.js
@@ -3,7 +3,17 @@ import express from "express"
 const transactionRouter = express.Router();
 
 // Create Transaction | POST
+transactionRouter.post('/create-transaction', (req, res) => {
+    // Require authentication for user validation
+    // get the user information to add transaction to respective user
+    // add transaction to transaction table with valid user id
+    // if success return 200
+    // else return error
+})
+
+// Fetch All Transactions | GET
+// Fetch One Transaction | GET
 // Update Transaction | PATCH
-//
+// Delete Transaction | DELETE
 
 export default transactionRouter;
--- schema/transactionSchema.js
+++ schema/transactionSchema.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const transactionSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
     description: {
         type: String,
     },
@@ -13,6 +17,10 @@ const transactionSchema = mongoose.Schema({
         type: mongoose.Schema.Types.Decimal128,
         required: true
     }
-})
+},
+{
+    timestamps: true
+}
+)
 
 export default transactionSchema;
