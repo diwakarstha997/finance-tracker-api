d6670c25dc871d356bd900c4947a85858100a35e
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-05-08 11:03:38 +1000

    Update create transaction enpoint

--- model/transactionModel.js
+++ model/transactionModel.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
-import transactionSchema from "../schema/transactionSchema";
+import transactionSchema from "../schema/transactionSchema.js";
 
 const transactionModel = mongoose.model("transaction", transactionSchema);
 
 // functions to perform operations with db
+
+export const createTransaction = (transactionObj) => {
+        return transactionModel(transactionObj).save();
+}
--- routers/tranasactionRouter.js
+++ routers/tranasactionRouter.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { verifyAccessJWT } from "../utility/jwtHelper.js";
 import { findUserByEmail } from "../model/userModel.js";
+import { createTransaction } from "../model/transactionModel.js";
 
 const transactionRouter = express.Router();
 
-// Create Transaction | POST
-transactionRouter.post('/create-transaction', async (req, res) => {
+// Create Transaction | POST)
+transactionRouter.post("/create-transaction", async (req, res) => {
     try {
         // Require authentication for user validation
         const { authorization } = req.headers;
@@ -27,13 +28,23 @@ transactionRouter.post('/create-transaction', async (req, res) => {
         // add transaction to transaction table with valid user id
         if(user?._id && user?.isEmailVerified){
             // call the model method to save trasaction to db
+            const transaction = await createTransaction({...req.body,userId: user._id});
+            console.log(transaction);
+            // if success return 200
+            if(transaction?._id){
+                res.json({
+                    status: "success",
+                    message: "Transaction created succesfully"
+                });
+            }
         }
 
-        // if success return 200
-        // else return error
         
     } catch (error) {
-        
+        res.json({
+            status: "error",
+            message: "Error:"+error
+        });
     }
 })
 
